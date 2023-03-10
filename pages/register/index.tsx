d155a327b3a0eeacbee51b4aff6a85caf4a830fd
amirsa025 <amirsaki06@gmail.com>
2023-03-10 18:59:39 +0330

    add heading to register

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Asideregister from "@/app/components/register/Asideregister";
 import MainSginup from "@/app/components/register/MainSginUp";
+import Heading from "@/app/components/Heading";
 
 const Register = () => {
     return (
         <>
             <div className={"grid grid-cols-1 lg:grid-cols-2 "}>
+                <Heading page={"ثبت نام"} titlesite={" کایا"}/>
                 <Asideregister/>
                 <MainSginup/>
             </div>
