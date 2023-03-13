7ac72db9afbad1fa036e39b86f5a41b597328a55
amirsa025 <amirsaki06@gmail.com>
2023-03-13 18:38:26 +0330

    add react query and fix bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,33 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Container from "@/app/components/layout/Container";
 import 'remixicon/fonts/remixicon.css'
-import {ReactElement, ReactNode} from "react";
+import React, {ReactElement, ReactNode} from "react";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {NextPage} from "next";
+import {
+    Hydrate,
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
     getLayout?: (page: ReactElement) => ReactNode
 }
 type AppPropsWithLayout = AppProps & {
     Component: NextPageWithLayout
 }
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+export default function App({Component, pageProps}: AppPropsWithLayout) {
     const getLayout = Component.getLayout ?? ((page) => page)
-    return getLayout( <Container><Component {...pageProps} /></Container>)
+    const [queryClient] = React.useState(() => new QueryClient())
+    return getLayout(
+        <Container>
+            <QueryClientProvider client={queryClient}>
+                <Hydrate state={pageProps.dehydratedState}>
+                    <Component {...pageProps} />
+                </Hydrate>
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </Container>
+    )
+
 }
--- pages/user-panel/index.tsx
+++ pages/user-panel/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NextPageWithLayout} from "@/pages/_app";
 import UserPanelAdmin from "@/app/components/User-panel-admin";
-import Link from "next/link";
 import Cookies from "universal-cookie";
+import Router from "next/router";
 
 const UserPanelPage:NextPageWithLayout = () => {
+    const [loading,setloading] = useState(true)
+    useEffect(()=>{
+        setTimeout(()=>setloading(false),1000)
+    },[])
+    if(loading) return <div>loading...</div>
     const cookie = new Cookies()
     if(!cookie.get('token')){
-        return  (
-            <div className={"container-app flex flex-col"}>
-                <Link href={"/"} className={"text-red-400"}>بازگشت به خانه </Link>
-                شما به این صفحه دسترسی ندارید
-
-            </div>
-
-        )
+        Router.replace('/').then()
+        return <></>
     }
+
     return (
         <>
             userPanelAdmin
