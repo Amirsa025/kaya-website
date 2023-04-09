c6fd1f05cebc1679555fe956871b87d7d5fe3614
amirsa025 <amirsaki06@gmail.com>
2023-04-09 12:30:23 +0430

    edit toast in project

--- app/components/form/login/AsideLogin.tsx
+++ app/components/form/login/AsideLogin.tsx
@@ -4,13 +4,7 @@ import Image from "next/image";
 const AsideLogin = () => {
     return (
         <>
-            <div className={"flex flex-row-reverse lg:flex-col justify-between lg:h-screen"}
-                 data-aos="fade-down"
-                 data-aos-anchor="#example-anchor"
-                 data-aos-easing="linear"
-                 data-aos-duration="1000"
-                 data-aos-offset="750"
-            >
+            <div className={"flex flex-row-reverse lg:flex-col justify-between lg:h-screen"}>
                 <div>
                     <Image src={"/images/login kaya.webp"} alt={""} height={800} width={800} className={"hidden lg:block"}/>
                     <Image src={"/images/loginresponsive.webp"} alt={""} height={200} width={200} className={"hidden md:flex lg:hidden"}/>
