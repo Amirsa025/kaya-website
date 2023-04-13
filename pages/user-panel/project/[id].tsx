70cfe4614da25221e96d52c5550ac1b0ce25293b
amirsa025 <amirsaki06@gmail.com>
2023-04-13 14:05:45 +0430

    fixed bug in image tag

--- pages/user-panel/project/[id].tsx
+++ pages/user-panel/project/[id].tsx
@@ -1,46 +1,46 @@
 import React from 'react';
-import callApi from "@/app/helper/callApi";
-import Cookies from "universal-cookie";
-import {dehydrate} from "@tanstack/query-core";
-import {QueryClient, useQuery} from "@tanstack/react-query";
-import {useRouter} from "next/router";
-import {GetStaticPaths, GetStaticProps} from "next";
+// import callApi from "@/app/helper/callApi";
+// import Cookies from "universal-cookie";
+// import {dehydrate} from "@tanstack/query-core";
+// import {QueryClient, useQuery} from "@tanstack/react-query";
+// import {useRouter} from "next/router";
+// import {GetStaticPaths, GetStaticProps} from "next";
 import Header from "@/app/shared/NavBar";
-export const fetchProjects = async (id: any) => {
-    const cookie = new  Cookies()
-    try {
-        return await callApi().get(`/projects/projects?limit=5&offset=8`, {
-            headers: {
-                'Authorization': `Bearer ${cookie.get('sginUP') || cookie.get('token')}`
-            }
-        })
-    } catch (error) {
-        console.error(error)
-    }
-}
+// export const fetchProjects = async (id: any) => {
+//     const cookie = new  Cookies()
+//     try {
+//         return await callApi().get(`/projects/projects?limit=5&offset=8`, {
+//             headers: {
+//                 'Authorization': `Bearer ${cookie.get('sginUP') || cookie.get('token')}`
+//             }
+//         })
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
 const SendBid = () => {
-    const router =useRouter()
-    const ProjectId = typeof router.query?.id === "string" ? router.query.id : "";
-    const { isSuccess, data, isLoading, isError } = useQuery(
-        ["getSendData", ProjectId],
-        () => fetchProjects(ProjectId),
-        {
-            enabled: ProjectId.length > 0,
-            staleTime: Infinity
-        }
-    );
-    if(isError){
-        return (
-            <div className="center">
-                We could not find your ProjectList
-                <span role="img" aria-label="sad">
-        </span>
-            </div>
-        );
-    }
-    if (isLoading) {
-        return <div className="center">Loading...</div>;
-    }
+    // const router =useRouter()
+    // const ProjectId = typeof router.query?.id === "string" ? router.query.id : "";
+    // const { isSuccess, data, isLoading, isError } = useQuery(
+    //     ["getSendData", ProjectId],
+    //     () => fetchProjects(ProjectId),
+    //     {
+    //         enabled: ProjectId.length > 0,
+    //         staleTime: Infinity
+    //     }
+    // );
+    // if(isError){
+    //     return (
+    //         <div className="center">
+    //             We could not find your ProjectList
+    //             <span role="img" aria-label="sad">
+    //     </span>
+    //         </div>
+    //     );
+    // }
+    // if (isLoading) {
+    //     return <div className="center">Loading...</div>;
+    // }
 
     return (
         <>
@@ -53,27 +53,27 @@ const SendBid = () => {
 export default SendBid;
 
 
-export const getStaticProps: GetStaticProps = async (context:any) => {
-    const { id } = context.params;
-    const queryClient = new QueryClient();
-
-    try {
-        await queryClient.fetchQuery(['[project]', id], () => fetchProjects(id))
-    } catch (error:any) {
-        context.res.statusCode = error.response.status;
-    }
-
-    return {
-        props: {
-            //also passing down isError state to show a custom error component.
-            dehydratedState: dehydrate(queryClient),
-        },
-    }
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-    return {
-        paths: [],
-        fallback: "blocking"
-    };
-};
+// export const getStaticProps: GetStaticProps = async (context:any) => {
+//     const { id } = context.params;
+//     const queryClient = new QueryClient();
+//
+//     try {
+//         await queryClient.fetchQuery(['[project]', id], () => fetchProjects(id))
+//     } catch (error:any) {
+//         context.res.statusCode = error.response.status;
+//     }
+//
+//     return {
+//         props: {
+//             //also passing down isError state to show a custom error component.
+//             dehydratedState: dehydrate(queryClient),
+//         },
+//     }
+// };
+//
+// export const getStaticPaths: GetStaticPaths = async () => {
+//     return {
+//         paths: [],
+//         fallback: "blocking"
+//     };
+// };
